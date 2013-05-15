d8a05d2dca4323eae3715cba4aedfdfba76eabfd
Pulkit Sethi <sethi.pulkit@gmail.com>
2013-05-14 20:26:32 -0400

    Added blog page, fixed top navbar, added routes

--- app.js
+++ app.js
@@ -4,30 +4,36 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
+  , site = require('./routes/site')
+  , blog = require('./routes/blog')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , consolidate = require('consolidate');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.engine('html', consolidate.handlebars);
+app.set('view engine', 'html');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));	//Removes "public" from url
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//URL mapping
+app.get('/', site.index);
+app.get('/blog?:format', blog.list);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
