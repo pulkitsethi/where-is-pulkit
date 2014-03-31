3e720b455591a42d3debd3667bcfbb9a6d0eb67d
Pulkit Sethi <sethi.pulkit@gmail.com>
2014-03-31 00:35:12 -0400

    Cleaned up and consolidated public resources (stylesheet, javascript, images)

--- models/user.js
+++
@@ -1,9 +0,0 @@
-/** User Schema for CrowdNotes **/
-
-var glue = require('passport-glue');
-
-var userSchema = glue.userSchema;
-userSchema.plugin(glue.github_oauth2_plugin);
-userSchema.plugin(glue.persistent_sessions_plugin);
-
-mongoose.model('User', userSchema);
