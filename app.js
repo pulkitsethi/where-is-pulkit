bfef83ea8a737b226efb528789941d9d188e1e6d
Pulkit Sethi <sethi.pulkit@gmail.com>
2014-04-04 01:41:19 -0400

    Production settings for Socket.io

--- app.js
+++ app.js
@@ -52,6 +52,25 @@ var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
 
+// Prodcution settings for socket.io
+if ('production' == app.get('env')) {
+    io.enable('browser client minification');  // send minified client
+    io.enable('browser client etag');          // apply etag caching logic based on version number
+    io.enable('browser client gzip');          // gzip the file
+    io.set('log level', 1);                    // reduce logging
+
+    // enable all transports (optional if you want flashsocket support, please note that some hosting
+    // providers do not allow you to create servers that listen on a port different than 80 or their
+    // default port)
+    io.set('transports', [
+        'websocket'
+      , 'flashsocket'
+      , 'htmlfile'
+      , 'xhr-polling'
+      , 'jsonp-polling'
+    ]);
+}
+
 // Setup routes
 require('./routes')(app, server);
 
