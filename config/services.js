cf28596c3b53bfc44f6169c0372e2cb49c86ad61
Pulkit Sethi <sethi.pulkit@gmail.com>
2014-04-05 21:09:05 -0400

    3rd party API keys

---
+++ config/services.js
@@ -0,0 +1,19 @@
+var ids = {
+    foursquare: {
+        oauth_token: "",
+        user_id: ""
+    },
+    flickr: {
+        api_key: "",
+        photoset_id: ""
+    },
+    twitter: {
+        key: ""
+    },
+    mongodb: {
+        dev: "",
+        prod: ""
+    }
+}
+
+module.exports = ids;
